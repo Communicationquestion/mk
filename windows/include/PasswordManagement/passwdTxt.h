96ae794b3a046698a70e4cdc19520bed40a87a8b
xk <xk@DESKTOP-L6CB8LA>
2024-09-05 17:34:03 +0800

    1.9.5

--- windows/include/PasswordManagement/passwdTxt.h
+++ windows/include/PasswordManagement/passwdTxt.h
@@ -7,18 +7,24 @@ namespace edtxt {
 	class edtxtfile
 	{
 	public:
-
 		std::vector<T> txttoitem(std::string&& str) {
-			for (auto& i : str) {
-				//T.name = i;
-				std::cout << i;
+			std::stringstream ss(str);
+			std::string line;
+			while (std::getline(ss, line, '\n')) {
+				auto name = [&line]()->std::string {
+					//取得空格前的字串
+					auto pos = line.find(' ');
+					if (pos != std::string::npos) {
+						return line.substr(0, pos);
+					}
+					}; 
+
+				//auto user = 
+				//auto passwd = 
 			}
 			return std::vector<T>{};
 		}
 	private:
 
 	};
-
-
-
 }
