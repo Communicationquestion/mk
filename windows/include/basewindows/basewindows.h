27c65e2f155ab6305ae01b12fd2699193cfbc429
Your Name <you@example.com>
2024-12-08 17:56:54 +0800

    删除qt 合并

--- windows/include/basewindows/basewindows.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-
-int initqmlapp(int argc, char* argv[]);
--- windows/include/wbg/global.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-struct Account {
-	QString name;
-	QString user;
-	QString passwd;
-
-};
-extern std::vector<Account>* myenv;
