e24cb556e46dfdbb8f71d390e18a5b80e5a063b6
Your Name <you@example.com>
2025-04-04 20:50:25 +0800

    FIX CMAKE

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -8,33 +8,37 @@
 #include <optional>
 
 int main_test(std::vector<std::string> cmd_strs) {
-  std::vector<std::string> v_commands{};
-  for (auto &cmd_str : cmd_strs) {
-    v_commands.push_back(cmd_str);
-  }
-  std::string _cmd = v_commands.at(0);
-  MkCommandStrategy::CMKCommandStrategy test(_cmd);
-  test.initmap();
-  test.execute(v_commands);
-  return 0;
+	std::vector<std::string> v_commands{};
+	for(auto& cmd_str : cmd_strs) {
+		v_commands.push_back(cmd_str);
+	}
+	std::string _cmd = v_commands.at(0);
+	MkCommandStrategy::CMKCommandStrategy test(_cmd);
+	test.initmap();
+	test.execute(v_commands);
+	return 0;
 }
 int imgui_test() {
 
-  TestApp tapp;
+	TestApp tapp;
 
-  tapp.app_run();
+	tapp.app_run();
 
-  return 0;
+	return 0;
 }
-namespace {
-TEST(Imgui, PositiveNumbers) { EXPECT_EQ(imgui_test(), 0); }
-TEST(Transltate, PositiveNumbers) {
-  EXPECT_EQ(main_test({"enzh", "hello"}), 0);
-  EXPECT_EQ(main_test({"zhen", "你好"}), 0);
-}
+namespace
+{
+	TEST(Imgui, PositiveNumbers) {
+		EXPECT_EQ(imgui_test(), 0);
+	}
+	TEST(Transltate, PositiveNumbers) {
+		EXPECT_EQ(main_test({"enzh", "hello"}), 0);
+		EXPECT_EQ(main_test({"zhen", "你好"}), 0);
+	}
 } // namespace
-int main(int argc, char *argv[]) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-  return 0;
+
+int main(int argc, char* argv[]) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+	return 0;
 }
